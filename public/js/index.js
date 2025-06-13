77061103101e864723926e763dc84ca4ddb2424e
Dakota McMullin <kotamc13@gmail.com>
2025-06-13 10:35:06 -0400

    Updated CSS based on feedback, and added Unsplash.

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 "use strict"
 
-const elements = {
+async function getRandomImage() {
+    const client_id = "Ec2mkH_8kJq5rxCfRaG2XhL8M97ITuQ5G5uvNJITRIc";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getRandomImage();
+
+/* const elements = {
     quote: document.getElementById("quote"),
     author: document.getElementById("author"),
 };
@@ -45,4 +62,4 @@ function loopThroughQuotes() {
     }, 3000);
 }
 
-setTimeout(loopThroughQuotes, 3000);
+setTimeout(loopThroughQuotes, 3000); */
