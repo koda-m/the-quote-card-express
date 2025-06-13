850e4301aede41e1b3f5139f7dede7ad10e43f51
Dakota McMullin <kotamc13@gmail.com>
2025-06-13 10:03:12 -0400

    Created index.js.

---
+++ public/js/index.js
@@ -0,0 +1,48 @@
+"use strict"
+
+const elements = {
+    quote: document.getElementById("quote"),
+    author: document.getElementById("author"),
+};
+
+const quotes = [
+    {
+        quote: "All hands! Abandon ship!",
+        author: "Captain Picard",
+    },
+
+    {
+        quote: "Doh!",
+        author: "Homer Simpson",
+    },
+
+    {
+        quote: "The Internet is the first thing that humanity has built that humanity doesn't understand, the largest experiment in anarchy that we have ever had.",
+        author: "Eric Schmidt",
+    },
+
+    {
+        quote: "I'm not afraid of hard work. I just don't like it.",
+        author: "Bob Belcher",
+    },
+
+    {
+        quote: "Be good to people. Being good to people is a wonderful legacy to leave behind.",
+        author: "Taylor Swift",
+    }
+]
+
+function loopThroughQuotes() {
+    let quoteIndex = 0;
+    setInterval(() => {
+        if (quoteIndex < quotes.length) {
+            elements.quote.textContent = quotes[quoteIndex].quote;
+            elements.author.textContent = quotes[quoteIndex].author;
+            quoteIndex++;
+        } else {
+            quoteIndex = 0;
+        }
+    }, 3000);
+}
+
+setTimeout(loopThroughQuotes, 3000);
